bb4f2b83177157fa88cc5729748a027c5053abe6
exonintrendo <exonintrendo@gmail.com>
2014-09-10 18:44:46 -0400

    adjusted directories and added autoloader

--- app/public/index.php
+++ app/public/index.php
@@ -6,20 +6,22 @@
  * @license http://opensource.org/licenses/MIT MIT License
  */
 
-define('ROOT', dirname(dirname(dirname(__FILE__))));
+define('ROOT', dirname(dirname(__FILE__)));
 
 $domain = str_replace('www.', '', $_SERVER['SERVER_NAME']);
 if (file_exists(ROOT . '/app/Config/' . $domain . '.php')) {
     require_once(ROOT . '/app/Config/' . $domain . '.php');
 }
 else {
-    require_once(ROOT . '/app/Config/config.php');
+    require_once(ROOT . '/Config/config.php');
 }
 
 // checking for minimum PHP version
 if (version_compare(PHP_VERSION, '5.3.7', '<') ) {
     exit("Sorry, this framework does not run on a PHP version smaller than 5.3.7!");
 }
+
+require_once(ROOT . '/vendor/autoload.php');
 
 $app = new \Primer\Core\Application();
 $app->run();
