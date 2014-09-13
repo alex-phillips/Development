947d74a2771ee3db944f40f7c5a6dcd5fe7e671c
exonintrendo <exonintrendo@gmail.com>
2014-09-13 11:03:49 -0400

    removed version check - this is in the primer core now

--- app/public/index.php
+++ app/public/index.php
@@ -16,11 +16,6 @@ else {
     require_once(ROOT . '/app/Config/config.php');
 }
 
-// checking for minimum PHP version
-if (version_compare(PHP_VERSION, '5.3.7', '<') ) {
-    exit("Sorry, this framework does not run on a PHP version smaller than 5.3.7!");
-}
-
 require_once(ROOT . '/vendor/autoload.php');
 
 $app = new \Primer\Core\Application();
