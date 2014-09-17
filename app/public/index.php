ceae68bd71293d719f85defb99681f0872e287f2
exonintrendo <exonintrendo@gmail.com>
2014-09-16 22:28:08 -0400

    all major configs are now kept within the config.php file

--- app/public/index.php
+++ app/public/index.php
@@ -6,17 +6,23 @@
  * @license http://opensource.org/licenses/MIT MIT License
  */
 
-define('ROOT', dirname(dirname(dirname(__FILE__))));
+/**
+ * Configuration for: Folders
+ * There is no need to edit these unless the Primer folder is located in another
+ * directory.
+ */
+define('DS', DIRECTORY_SEPARATOR);
+define('APP_ROOT', dirname(dirname(__FILE__)));
+set_include_path(APP_ROOT);
 
-$domain = str_replace('www.', '', $_SERVER['SERVER_NAME']);
-if (file_exists(ROOT . '/app/Config/' . $domain . '.php')) {
-    require_once(ROOT . '/app/Config/' . $domain . '.php');
-}
-else {
-    require_once(ROOT . '/app/Config/config.php');
-}
+//define('ENVIRONMENT', 'test');
 
-require_once(ROOT . '/vendor/autoload.php');
+/**
+ * Configuration for: Timezone
+ */
+date_default_timezone_set('America/New_York');
+
+require_once(APP_ROOT . '/../vendor/autoload.php');
 
 $app = new \Primer\Core\Application();
 $app->run();
