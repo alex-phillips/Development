4aa7c0bd76e78ad4b47a0804c79ad4c40657ce6d
exonintrendo <exonintrendo@gmail.com>
2014-09-25 12:36:13 -0400

    moved shared (http and console) setup to start.php

---
+++ app/Config/start.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: exonintrendo
+ * Date: 9/24/14
+ * Time: 8:29 PM
+ */
+
+/**
+ * Configuration for: Folders
+ * There is no need to edit these unless the Primer folder is located in another
+ * directory.
+ */
+define('DS', DIRECTORY_SEPARATOR);
+define('APP_ROOT', dirname(dirname(__FILE__)));
+set_include_path(APP_ROOT);
+
+//define('ENVIRONMENT', 'test');
+
+/**
+ * Configuration for: Timezone
+ */
+date_default_timezone_set('America/New_York');
+
+require_once(APP_ROOT . '/../vendor/autoload.php');
--- app/public/index.php
+++ app/public/index.php
@@ -6,23 +6,7 @@
  * @license http://opensource.org/licenses/MIT MIT License
  */
 
-/**
- * Configuration for: Folders
- * There is no need to edit these unless the Primer folder is located in another
- * directory.
- */
-define('DS', DIRECTORY_SEPARATOR);
-define('APP_ROOT', dirname(dirname(__FILE__)));
-set_include_path(APP_ROOT);
-
-//define('ENVIRONMENT', 'test');
-
-/**
- * Configuration for: Timezone
- */
-date_default_timezone_set('America/New_York');
-
-require_once(APP_ROOT . '/../vendor/autoload.php');
+require_once('../Config/start.php');
 
 $app = new \Primer\Core\Application();
 $app->run();
