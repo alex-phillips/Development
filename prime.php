627da94550b5e6b94068edad993328a79df167f8
exonintrendo <exonintrendo@gmail.com>
2014-09-27 22:11:37 -0400

    fixed require path

--- prime.php
+++ prime.php
@@ -6,7 +6,7 @@
  * Time: 6:30 PM
  */
 
-require_once('./Config/start.php');
+require_once('./app/Config/start.php');
 
 $console = new \Primer\Console\Console('Prime', '0.1');
 $console->addCommand(new \Primer\Console\DownCommand(), array('down'));
