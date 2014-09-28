e4ca02636a2399f815309a488c580ec4bb55db74
exonintrendo <exonintrendo@gmail.com>
2014-09-27 21:58:23 -0400

    add name and versioning information

--- app/prime.php
+++ app/prime.php
@@ -8,9 +8,7 @@
 
 require_once('./Config/start.php');
 
-$app = new \Primer\Core\Application();
-
 $console = new \Primer\Console\Console('Prime', '0.1');
 $console->addCommand(new \Primer\Console\DownCommand(), array('down'));
 $console->addCommand(new \Primer\Console\UpCommand(), array('up'));
-$console->run($argv, $argc);
+$console->dispatch();
