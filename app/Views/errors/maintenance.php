4015ca65f7e9b42555b38c9f54fca38ef179b2c5
exonintrendo <exonintrendo@gmail.com>
2014-12-14 11:36:19 -0500

    maintenance output now is HTML5 compliant

--- app/Views/errors/maintenance.php
+++ app/Views/errors/maintenance.php
@@ -1,3 +1,18 @@
-<h1 style="color: red;">
-    The website is currently under maintenance. Please try again later.
-</h1>
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Wootalbes | Down for Maintenance</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!--    <link rel="apple-touch-icon" href="apple-touch-icon.png">-->
+    <!-- Place favicon.ico in the root directory -->
+</head>
+<body>
+    <h1 style="color: red;">
+        The website is currently under maintenance. Please try again later.
+    </h1>
+</body>
+</html>
