0eaa6aeaf83082880f1ea846bbc0562dc806d857
exonintrendo <exonintrendo@gmail.com>
2015-01-15 12:37:30 -0500

    added default routes

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -8,9 +8,17 @@
 
 Router::route('/', array('controller' => 'posts', 'action' => 'index'));
 Router::route('/user/:username', array('controller' => 'users', 'action' => 'view', 'username'));
-Router::route('/post/:slug', array('controller' => 'posts', 'action' => 'view', 'slug'));
+Router::route('/post/:slug', array('controller' => 'posts', 'action' => 'view'));
 Router::route('/login/', array('controller' => 'users', 'action' => 'login'));
 Router::route('/logout/', array('controller' => 'users', 'action' => 'logout'));
 Router::route('/register/', array('controller' => 'users', 'action' => 'add'));
 Router::route('/movies', array('controller' => 'pages', 'action' => 'movies'));
 Router::route('/api/', array('controller' => 'apis', 'action' => 'index'));
+Router::route('/admin', array('controller' => 'users', 'action' => 'view', 'admin'));
+
+/*
+ * Default Primer routes. If you do not want to support the default routes,
+ * remove these lines.
+ */
+Router::route('/:controller', array('action' => 'index'));
+Router::route('/:controller/:action/*');
